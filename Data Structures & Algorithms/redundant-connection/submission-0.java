5e110ad3c55bdc63951b0fadc29c0a8c15f64f68
Rishi Raj Singh <rishi2758@gmail.com>
2026-06-15 18:55:33 +0530

    Add: redundant-connection - submission-0

---
+++ Data Structures & Algorithms/redundant-connection/submission-0.java
@@ -0,0 +1,49 @@
+class Solution {
+    int[] uf;
+    int[] size;
+
+    public void init(int n) {
+        this.uf = new int[n];
+        this.size = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            uf[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    public boolean union(int a, int b) {
+        int rootA = root(a);
+        int rootB = root(b);
+
+        if (rootA == rootB) return false;
+
+        if (size[rootA] < size[rootB]) {
+            uf[rootA] = rootB;
+            size[rootB] += size[rootA];
+        } else {
+            uf[rootB] = rootA;
+            size[rootA] += size[rootB];
+        }
+        return true;
+    }
+
+    private int root(int a) {
+        while(a != uf[a]) {
+            int parent = uf[a];
+            uf[a]=uf[uf[parent]];
+            a = parent;
+        }
+        return a;
+    }
+
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+        init(n+1);
+        int[] ans = new int[2];
+        for (int[] edge: edges) {
+            if(!union(edge[0], edge[1])) ans = edge;
+        }
+        return ans;
+    }
+}
