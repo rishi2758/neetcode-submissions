e5c5f6d8020ceea21b6423e6c711243224048e43
Rishi Raj Singh <rishi2758@gmail.com>
2026-06-15 11:20:09 +0530

    Add: course-schedule-ii - submission-2

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-2.java
@@ -0,0 +1,81 @@
+class Solution {
+
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List[] adjList = new List[numCourses];
+        for (int course = 0; course < numCourses; course++) { 
+            adjList[course] = new ArrayList<Integer>();
+        }
+        for (int[] prereq: prerequisites) {
+            adjList[prereq[0]].add(prereq[1]);
+        }
+
+        int[] visited = new int[numCourses];
+        List<Integer> courseCollect = new ArrayList<>();
+        for (int course = 0; course < numCourses; course++) {
+            if (!dfs(course, adjList, visited, courseCollect)) {
+                courseCollect.clear();
+                break;
+            }
+        }
+        
+        int[] res = new int[courseCollect.size()];
+        for (int i = 0; i < courseCollect.size(); i++) {
+            res[i] = courseCollect.get(i);
+        }
+        return res;
+    }
+
+    private int[] _findOrder(int numCourses, int[][] prerequisites) { 
+        int[] inDegree = new int[numCourses];
+        List<Integer>[] adjList = new List[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            adjList[i] = new ArrayList<Integer>();
+        }
+        for (int[] prereq: prerequisites) {
+            adjList[prereq[0]].add(prereq[1]);
+            inDegree[prereq[1]]++;
+        }
+        Queue<Integer> q = new LinkedList<>();
+        
+        for (int course = 0; course < numCourses; course++) {
+            if (inDegree[course] == 0) q.add(course);
+        }
+        
+        int[] courseOrder = new int[numCourses];
+        int idx = 0;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int k = 0; k < size; k++) {
+                int course = q.poll();
+                courseOrder[idx++] = course;
+                for (int adj : adjList[course]) {
+                    if (inDegree[adj] == 0) {
+                        q.add(adj);
+                    } else {
+                        inDegree[adj]--;
+                    }
+                }
+            }
+        }
+        return idx == numCourses ? courseOrder : new int[] {};
+
+    }
+
+    private boolean dfs(int course, List<Integer>[] adjList, int[] visited, List<Integer> courseCollect) {
+        if (visited[course] == 1) return false;
+        if (visited[course] == 2) return true;
+
+        visited[course] = 1;
+        
+
+        for (int prereq: adjList[course]) {
+            if(!dfs(prereq, adjList, visited, courseCollect)) {
+                return false;
+            }
+        }
+
+        visited[course] = 2;
+        courseCollect.add(course);
+        return true;
+    }   
+}
